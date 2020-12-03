0ade6d467a0488f1767b5eb1fa3d7f9becf0bc35
Squirrel-314 <68402033+Squirrel-314@users.noreply.github.com>
2020-12-03 14:38:41 -0500

    hi

--- JavaScript/job.js
+++ JavaScript/job.js
@@ -2,13 +2,25 @@ let employer = ['Flimsy Constructs Inc.', 'Collapse Co.', 'Slapdash L.L.C.', 'Ri
 let budget = ['$10,000', '$8,000'][Math.floor(Math.random() * 2)];
 let waitTime = [8000, 12000, 10000][Math.floor(Math.random() * 2)];
 
+function accept() {
+   window.location.href = "../HTML/job-info.html";
+}
+
+function deny() {
+   document.getElementById("job").innerHTML = "You have no job offers.";
+   setTimeout(giveJob, waitTime);
+   document.getElementById("yes").style.display = "none";
+   document.getElementById("no").style.display = "none";
+}
+
 function giveJob() {
-   console.log(employer,"is hiring you for", budget);
-   document.getElementById("job1").innerHTML = employer + " is hiring you for " + budget;
+   document.getElementById("job").innerHTML = employer + " is hiring you for " + budget;
+   document.getElementById("yes").style.display = "inline-block";
+   document.getElementById("no").style.display = "inline-block";
 }
 
 function findJob() {
-   setTimeout(giveJob, waitTime)
+   setTimeout(giveJob, waitTime);
 }
 
 window.onload = findJob()
