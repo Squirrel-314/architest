1afcfc37a29bff0f0fc4f8d75ee8e51ac82809f9
Squirrel-314 <68402033+Squirrel-314@users.noreply.github.com>
2020-12-03 13:35:07 -0500

    hi

---
+++ JavaScript/job.js
@@ -0,0 +1,11 @@
+let employer = ['Flimsy Constructs Inc.', 'Collapse Co.', 'Slapdash L.L.C.', 'Rinkydink Mistakes Corp', 'Rickity Builds Ltd'][Math.floor(Math.random() * 5)];
+console.log(result);
+
+
+
+
+function timeCitation() {
+   setTimeout(citation, 10000)
+}
+
+window.onload = timeCitation()
