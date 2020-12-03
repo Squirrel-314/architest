b788c508212641c0e990053cc51545a353a01d40
Squirrel-314 <68402033+Squirrel-314@users.noreply.github.com>
2020-12-03 13:52:02 -0500

    Update job.js

--- JavaScript/job.js
+++ JavaScript/job.js
@@ -1,6 +1,6 @@
 let employer = ['Flimsy Constructs Inc.', 'Collapse Co.', 'Slapdash L.L.C.', 'Rinkydink Mistakes Corp', 'Rickity Builds Ltd'][Math.floor(Math.random() * 5)];
 let budget = ['$10,000', '$8,000'][Math.floor(Math.random() * 2)];
-let waitTime = [8000, 12000][Math.floor(Math.random() * 2)];
+let waitTime = [8000, 12000, 10000][Math.floor(Math.random() * 2)];
 
 function giveJob() {
    console.log(employer,"is hiring you for", budget);
