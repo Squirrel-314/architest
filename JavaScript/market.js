2252d49cfec522b72076e36eafb397d979097a2b
Squirrel-314 <68402033+Squirrel-314@users.noreply.github.com>
2020-12-05 11:30:12 -0500

    hi

--- JavaScript/market.js
+++ JavaScript/market.js
@@ -2,30 +2,134 @@
 // Varibles
 //============================================================
 
-let money = 12000;
+var money = 12000;
 
-var resouces {
+var resouces = {
+   mud: 0,
+   mudCost: 20,
    wood: 0,
+   woodCost: 50,
+   plastic: 0,
+   plasticCost: 100,
+   glass: 0,
+   glassCost: 120,
+   bricks: 0,
+   brickCost: 150,
    sandstone: 0,
+   sandstoneCost: 200,
    limestone: 0,
+   limestoneCost: 200,
+   iron: 0,
+   ironCost: 500,
+   marble: 0,
+   marbleCost: 700,
    titanium: 0,
+   titaniumCost: 5000,
 }
 
-var humanResouces {
+var humanResouces = {
    worker: 0,
+   workerPay: 0,
    overseer: 0,
+   overseerPay: 0,
+   professionalWorker: 0,
+   professionalWorkerPay: 0,
+   professionalOverseer: 0,
+   professionalOverseerPay: 0,
+   expertWorker: 0,
+   expertWorkerPay: 0,
+   expertOverseer: 0,
+   expertOverseerPay: 0,
+   robotWorker: 0,
+   robotWorkerPay: 0,
+   robotOverseer: 0,
+   robotOverseerPay: 0,
 }
 
 var moneyAndResouces = window.setInterval(function() {
-
-}, 2000)
+   document.getElementById("money").innerHTML = "$" + money;
+}, 500)
 
 //============================================================
 // Buy Materials
 //============================================================
 
-
+function buyMud() {
+   if (money >= resouces.mudCost){
+      money -= resouces.mudCost;
+      resouces.mud += 5;
+   }
+}
+function buyWood() {
+   if (money >= resouces.woodCost){
+      money -= resouces.woodCost;
+      resouces.wood += 5;
+   }
+}
+function buyPlastic() {
+   if (money >= resouces.plasticCost){
+      money -= resouces.plasticCost;
+      resouces.plastic += 5;
+   }
+}
+function buyGlass() {
+   if (money >= resouces.glassCost){
+      money -= resouces.glassCost;
+      resouces.glass += 5;
+   }
+}
+function buyBricks() {
+   if (money >= resouces.bricksCost){
+      money -= resouces.bricksCost;
+      resouces.bricks += 5;
+   }
+}
+function buySandstone() {
+   if (money >= resouces.sandstoneCost){
+      money -= resouces.sandstoneCost;
+      resouces.sandstone += 5;
+   }
+}
+function buyLimestone() {
+   if (money >= resouces.limestoneCost){
+      money -= resouces.limestoneCost;
+      resouces.limestone += 5;
+   }
+}
+function buyIron() {
+   if (money >= resouces.ironCost){
+      money -= resouces.ironCost;
+      resouces.iron += 5;
+   }
+}
+function buyMarble() {
+   if (money >= resouces.marbleCost){
+      money -= resouces.marbleCost;
+      resouces.marble += 5;
+   }
+}
+function buyTitanium() {
+   if (money >= resouces.titaniumCost){
+      money -= resouces.titaniumCost;
+      resouces.titanium += 5;
+   }
+}
 
 //============================================================
 // Hire Workers
 //============================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Like limestone, sandstone is a sedimentary rock, formed by the settling and compression of materials over time. ... While limestone's density makes it strong under heavy pressure, sandstone is harder and provides better resistance against heat and impact
